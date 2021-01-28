74ed09f861cb33404000a430c20cbe18b033c29f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-01-28 11:30:04 +0800

    Your commit message

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,42 @@ import { Button, Box, Container, Heading, Text } from 'theme-ui'
 
 const App = () => {
   return (
-    <Box
-      sx={{
-        background: `linear-gradient(
+    <Container p={0} m={0}>
+      <Box
+        sx={{
+          background: `linear-gradient(
       0deg,
       rgba(0, 0, 0, 0.74),
       rgba(25,25,60, 0.74)
     ),url(https://i.gifer.com/HseP.gif)`,
-        backgroundSize: 'cover',
-        color: 'white',
-        fontSize: '1.3rem!important',
-      }}
-    >
-      <Container as="main" py={4} variant="copy">
-        <Heading sx={{ fontSize: '3rem' }}>
-          This is what{' '}
-          <Heading sx={{ fontSize: '7rem', marginTop: '-0.2em' }}>
-            <Text sx={t => t.util.gxText('cyan', 'blue')}>we made.</Text>
+          backgroundSize: 'cover',
+          color: 'white',
+          fontSize: '1.3rem!important',
+        }}
+      >
+        <Container py={4} variant="copy">
+          <Heading sx={{ fontSize: '3rem' }}>
+            This is what{' '}
+            <Heading sx={{ fontSize: '7rem', marginTop: '-0.2em' }}>
+              <Text sx={t => t.util.gxText('cyan', 'blue')}>we made.</Text>
+            </Heading>
           </Heading>
-        </Heading>
-        <Text
-          sx={{
-            fontSize: '1.3rem!important',
-            fontWeight: '600',
-            marginTop: '5px',
-          }}
-        >
-          {' '}
-          MYP Personal Project Exhibition 2021
-        </Text>
+          <Text
+            sx={{
+              fontSize: '1.3rem!important',
+              fontWeight: '600',
+              marginTop: '5px',
+            }}
+          >
+            {' '}
+            MYP Personal Project Exhibition 2021
+          </Text>
+        </Container>
+      </Box>
+      <Container py={4} variant="copy">
+        Every year we've seen our older peers produce products
       </Container>
-    </Box>
+    </Container>
   )
 }
 
