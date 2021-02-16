f1d878cc46ef5874f2c99463ee34e2293eb0f2b5
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-02-16 14:21:41 +0800

    Update global-context.js

--- components/global-context.js
+++ components/global-context.js
@@ -2,29 +2,55 @@ import { Button, Box, Container, Image, Grid, Heading, Text } from 'theme-ui'
 import Emoji from './emoji'
 import Link from 'next/link'
 
-let globalContext = ({ name, emoji, slug }) => (
-  <Link href={slug ? `context/${slug}` : ''}>
-    <Box
-      sx={{
-        bg: '#005a85',
-        borderRadius: '8px',
-        padding: '16px',
-        cursor: slug ? 'pointer' : 'default',
+function final({ name, emoji, slug }) {
+  return (
+    <Box>
+      {slug ? (
+        <Link href={`context/${slug}`}>
+          <Box
+            sx={{
+              bg: '#005a85',
+              borderRadius: '8px',
+              padding: '16px',
+              cursor: slug ? 'pointer' : 'default',
 
-        '&:hover': {
-          bg: 'rgba(0, 90, 133, 0.8)',
-        },
-      }}
-      className="global-context"
-    >
-      <Text>
-        <Emoji emoji={emoji} />
-        <span style={{ marginLeft: '8px' }}>
-          <b>{name}</b>
-        </span>
-      </Text>
+              '&:hover': {
+                bg: 'rgba(0, 90, 133, 0.8)',
+              },
+            }}
+            className="global-context"
+          >
+            <Text>
+              <Emoji emoji={emoji} />
+              <span style={{ marginLeft: '8px' }}>
+                <b>{name}</b>
+              </span>
+            </Text>
+          </Box>
+        </Link>
+      ) : (
+        <Box
+          sx={{
+            bg: '#005a85',
+            borderRadius: '8px',
+            padding: '16px',
+            cursor: slug ? 'pointer' : 'default',
+
+            '&:hover': {
+              bg: 'rgba(0, 90, 133, 0.8)',
+            },
+          }}
+          className="global-context"
+        >
+          <Text>
+            <Emoji emoji={emoji} />
+            <span style={{ marginLeft: '8px' }}>
+              <b>{name}</b>
+            </span>
+          </Text>
+        </Box>
+      )}
     </Box>
-  </Link>
-)
-
-export default globalContext
+  )
+}
+export default final
