b4db220d063456e2dd0da035befa7530b03c4163
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-03-03 16:42:03 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,41 +1,42 @@
 import { Box, Grid, Container, Heading, Text } from 'theme-ui'
 
 let footer = () => (
-<Box bg="blue" color="white">
-  <Container py={4}>
-    <Grid columns={2}>
-      <Heading sx={{ fontSize: '18px' }}>
-        MYP Personal Project Exhibition 2021
-      </Heading>
-      <Heading
-        sx={{ fontSize: '16px', fontWeight: '400', textAlign: 'right' }}
-      >
-        Website by{' '}
-        <a
-          href="https://github.com/sampoder"
-          style={{
-            color: 'white',
-            fontWeight: '600',
-            textDecoration: 'none',
-          }}
+  <Box bg="blue" color="white">
+    <Container py={4}>
+      <Grid columns={[1, 2, 2]}>
+        <Heading sx={{ fontSize: '18px' }}>
+          MYP Personal Project Exhibition 2021
+        </Heading>
+        <Heading
+          sx={{ fontSize: '16px', fontWeight: '400', textAlign: ['left', 'right', 'right'] }}
         >
-          @sampoder
-        </a>
-        , open sourced{' '}
-        <a
-          href="https://github.com/sampoder/personal-project-showcase"
-          style={{
-            color: 'white',
-            fontWeight: '600',
-            textDecoration: 'none',
-          }}
-        >
-          here
-        </a>
-        .
-      </Heading>
-    </Grid>
-  </Container>
-</Box>)
+          Website by{' '}
+          <a
+            href="https://github.com/sampoder"
+            style={{
+              color: 'white',
+              fontWeight: '600',
+              textDecoration: 'none',
+            }}
+          >
+            @sampoder
+          </a>
+          , open sourced{' '}
+          <a
+            href="https://github.com/sampoder/personal-project-showcase"
+            style={{
+              color: 'white',
+              fontWeight: '600',
+              textDecoration: 'none',
+            }}
+          >
+            here
+          </a>
+          .
+        </Heading>
+      </Grid>
+    </Container>
+  </Box>
+)
 
 export default footer
