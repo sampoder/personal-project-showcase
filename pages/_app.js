3ad44746b398cce1f98c18eed680a30d931793b4
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-03-22 14:47:48 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,9 @@ import { ThemeProvider } from 'theme-ui'
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <script async defer data-website-id="2fbff7ec-df32-4f0a-9ca9-39df87a38036" src="https://umami-pp-analytics.vercel.app/umami.js"></script>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
